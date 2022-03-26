504be1abd49c9488bbb62112caf8edae534f8442
deveckor <deveckor@gmail.com>
2022-03-25 23:28:02 -0600

    Add change in package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const server = require('./src/server');
 const dbConnect = require('./src/lib/bd');
 const req = require('express/lib/request');
-const port = process.env.Port || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 
@@ -10,8 +10,8 @@ const connect = async () =>{
     try {
         await dbConnect();
         console.log('Database connected :D');
-        server.listen(port, ()=>{
-            console.log(`Server running on port ${port}`);
+        server.listen(PORT, ()=>{
+            console.log(`Server running on port ${PORT}`);
         });
     } catch (err) {
         console.log(`Error: ${err}`);
